579397f15a28cabd645dd3fdaae123b8cdd590e5
Haroon-AL <haroon@andinolabs.com>
2025-10-08 13:37:36 +0530

    chore: added configservice to load dotenv variables

--- src/datasource/data-source.ts
+++ src/datasource/data-source.ts
@@ -1,20 +1,23 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 import { Cart } from '../entity/Cart';
 import { CartItem } from '../entity/CartItem';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { Products } from '../entity/Products';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const configService = new ConfigService();
 
 export const DatabaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'products',
+  host: configService.get<string>('DB_HOST', 'localhost'),
+  port: parseInt(configService.get<string>('DB_PORT', '5432')),
+  username: configService.get<string>('DB_USER', 'postgres'),
+  password: configService.get<string>('DB_PASSWORD', 'postgres'),
+  database: configService.get<string>('DB_NAME', 'products'),
   synchronize: false,
   logging: false,
-  entities: [Cart, CartItem,Products],
+  entities: [Cart, CartItem, Products],
   migrations: ['src/migrations/**/*.ts'],
 };
 
